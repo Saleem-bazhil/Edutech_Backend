ac920bef0637da4b6063aae72364119112a3b789
saleem <ab588538@gmail.com>
2025-12-06 21:35:44 +0530

    update the main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from core.database import Base, engine
-from api_routes.chat import router as chat_router 
+from api_routes.chat import router as chat_router
 from core.config import settings
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "https://your-frontend-domain.com",
+    "https://edutech-frontend-inky.vercel.app",  
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # React dev URL
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,8 +26,13 @@ def startup():
     
 api_prefix = settings.API_V1_PREFIX
 
-app.include_router(chat_router,prefix=api_prefix)
+app.include_router(chat_router, prefix=api_prefix)
 
-@app.get('/')
-def Home():
-    return {"message":"chat"}
+@app.get("/")
+def home():
+    return {"message": "chat"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=6001, reload=True)
+
